df91c160481097050ab60551e40cb19ac5fce29a
imacubsfan23 <imacubsfan23@gmail.com>
2016-12-25 14:32:55 -0600

    Add files via upload

---
+++ PigeonPuncher.py
@@ -0,0 +1,21 @@
+#imports
+import pygame
+pygame.display.set_caption('PigeonPuncher')
+
+#initializing and variables
+BLUE = (0,0,255)
+pygame.Surface.fill(BLUE)
+
+#classes and objects
+def Player(self):
+    self.pos_x = 0
+    self.change_y = 0
+    def calc_gravity(self):
+    
+#game loop
+def main():
+    pygame.display.flip()
+    
+#play until quit
+if __name__ == "__main__":
+    main()
