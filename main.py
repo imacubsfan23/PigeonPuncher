266a7178088bac1bade72a84aa5b328069218fb7
Tim Dickeson <imacubsfan23@gmail.com>
2017-01-07 14:22:42 -0600

    fixed scrolling issue and blit Player to the screen

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ temp_bg = pygame.image.load(bg_path)
 temp_bg_rect = temp_bg.get_rect()
 
 size = screen_width, screen_height = 1024, 768 #must be 1024,768
+floor = screen_height * 0.76
 screen = pygame.display.set_mode(size)
 x = 0
 x1 = screen_width
 y = 0
-scroll_speed = 20
+scroll_speed = 15
 
 def UpdateBackground():
         global x, x1, y, scroll_speed
@@ -22,15 +23,17 @@ def UpdateBackground():
         x1 -= scroll_speed
         screen.blit(temp_bg,(x,y))
         screen.blit(temp_bg,(x1,y))
-        if x < -screen_width:
-                x = screen_width
-        if x1 < -screen_width:
-                x1 = screen_width
+        if x <= -screen_width:
+                x = screen_width-scroll_speed
+        if x1 <= -screen_width:
+                x1 = screen_width-scroll_speed
 
 #-----Anderson Walking Animations-----#
 walk1_path = os.path.join("images", "temp_walk1.png")
 temp_walk1 = pygame.image.load(walk1_path)
 temp_walk1_rect = temp_walk1.get_rect()
+
+'''ATTENTION!!! WALK1 AND WALK2 MUST BE SAME SIZE FOR THIS TO WORK'''
 
 walk2_path = os.path.join("images", "temp_walk2.png")
 temp_walk2 = pygame.image.load(walk2_path)
@@ -58,10 +61,25 @@ temp_pigeon2_rect = temp_pigeon2.get_rect()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.images = [temp_walk1, temp_walk2]
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(0, 0, 75, 80)
+        
+        self.pos_x = screen_width * 0.1
+        self.pos_y = floor
+        self.change_y = 0
 
     def update(self):
         #blit Anderson where gravity and jump say he is
-        screen.blit(temp_walk1, temp_walk1_rect)        
+        self.rect = (self.pos_x,self.pos_y)
+        screen.blit(self.image, self.rect)
+        if self.pos_y > floor:
+                self.pos_y = floor
+        self.index += 1
+        if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
 
 '''class Pigeon(pygame.sprite.Sprite):
     def __init__(self):
@@ -86,4 +104,5 @@ while gameRunning:
                         pygame.quit()
                         sys.exit()
         UpdateBackground()
+        Player.update(Player())
         pygame.display.flip()
