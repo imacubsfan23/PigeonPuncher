7433d37aa6fe60568254f02484dc6f7e0e9d2c76
Tim Dickeson <imacubsfan23@gmail.com>
2017-01-07 15:46:56 -0600

    renamed pictures, added player animations (except for punching, because no art), and organized code with comments

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ pygame.display.set_caption('Pigeon Puncher')
 __author__ = "Tim Dickeson II"
 
 #-----Background-----#
-bg_path = os.path.join("images", "temp_bg.jpg")
-temp_bg = pygame.image.load(bg_path)
-temp_bg_rect = temp_bg.get_rect()
+bg_path = os.path.join("images", "bg.jpg")
+bg = pygame.image.load(bg_path)
+bg_rect = bg.get_rect()
 
 size = screen_width, screen_height = 1024, 768 #must be 1024,768
 floor = screen_height * 0.76
@@ -18,74 +18,84 @@ scroll_speed = 15
 
 def UpdateBackground():
         global x, x1, y, scroll_speed
-        screen.blit(temp_bg, temp_bg_rect)
+        screen.blit(bg, bg_rect)
         x -= scroll_speed
         x1 -= scroll_speed
-        screen.blit(temp_bg,(x,y))
-        screen.blit(temp_bg,(x1,y))
+        screen.blit(bg,(x,y))
+        screen.blit(bg,(x1,y))
         if x <= -screen_width:
-                x = screen_width-scroll_speed
+                x = screen_width-scroll_speed #fixes background scrolling issue
         if x1 <= -screen_width:
                 x1 = screen_width-scroll_speed
 
 #-----Anderson Walking Animations-----#
-walk1_path = os.path.join("images", "temp_walk1.png")
-temp_walk1 = pygame.image.load(walk1_path)
-temp_walk1_rect = temp_walk1.get_rect()
-
 '''ATTENTION!!! WALK1 AND WALK2 MUST BE SAME SIZE FOR THIS TO WORK'''
+walk1_path = os.path.join("images", "walk1.png")
+walk1 = pygame.image.load(walk1_path)
 
-walk2_path = os.path.join("images", "temp_walk2.png")
-temp_walk2 = pygame.image.load(walk2_path)
-temp_walk2_rect = temp_walk2.get_rect()
+walk2_path = os.path.join("images", "walk2.png")
+walk2 = pygame.image.load(walk2_path)
 
 #-----Anderson Jetpack Animations-----#
-jetpack_off_path = os.path.join("images", "temp_jetpack_off.png")
-temp_jetpack_off = pygame.image.load(jetpack_off_path)
-temp_jetpack_off_rect = temp_jetpack_off.get_rect()
+jetpack_off_path = os.path.join("images", "jetpack_off.png")
+jetpack_off = pygame.image.load(jetpack_off_path)
+jetpack_off_rect = jetpack_off.get_rect()
 
-jetpack_on_path = os.path.join("images", "temp_jetpack_on.png")
-temp_jetpack_on = pygame.image.load(jetpack_on_path)
-temp_jetpack_on_rect = temp_jetpack_on.get_rect()
+jetpack_on_path = os.path.join("images", "jetpack_on.png")
+jetpack_on = pygame.image.load(jetpack_on_path)
+jetpack_on_rect = jetpack_on.get_rect()
 
 #-----Pigeon Flying Animations-----#
-pigeon1_path = os.path.join("images", "temp_pigeon1.png")
-temp_pigeon1 = pygame.image.load(pigeon1_path)
-temp_pigeon1_rect = temp_pigeon1.get_rect()
+pigeon1_path = os.path.join("images", "pigeon1.png")
+pigeon1 = pygame.image.load(pigeon1_path)
+pigeon1_rect = pigeon1.get_rect()
 
-pigeon2_path = os.path.join("images", "temp_pigeon2.png")
-temp_pigeon2 = pygame.image.load(pigeon2_path)
-temp_pigeon2_rect = temp_pigeon2.get_rect()
+pigeon2_path = os.path.join("images", "pigeon2.png")
+pigeon2 = pygame.image.load(pigeon2_path)
+pigeon2_rect = pigeon2.get_rect()
 
 #classes and objects
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.images = [temp_walk1, temp_walk2]
+        self.images = [walk1, walk2, jetpack_on, jetpack_off]
         self.index = 0
+        self.firstIndex = 0
+        self.lastIndex = 1
         self.image = self.images[self.index]
-        self.rect = pygame.Rect(0, 0, 75, 80)
+        self.rect = pygame.Rect(0, 0, 64, 64)
         
         self.pos_x = screen_width * 0.1
         self.pos_y = floor
         self.change_y = 0
 
     def update(self):
-        #blit Anderson where gravity and jump say he is
+        if self.pos_y >= floor:
+            self.index = 0
+            self.firstIndex = 0
+            self.lastIndex = 1
+        else:
+            if self.change_y < 0:
+                self.index = 2
+                self.firstIndex = 2
+                self.lastIndex = 2
+            elif self.change_y > 0:
+                self.index = 3
+                self.firstIndex = 3
+                self.lastIndex = 3
+        self.image = self.images[self.index]
         self.rect = (self.pos_x,self.pos_y)
         screen.blit(self.image, self.rect)
         if self.pos_y > floor:
                 self.pos_y = floor
         self.index += 1
-        if self.index >= len(self.images):
-                self.index = 0
-        self.image = self.images[self.index]
+        if self.index > self.lastIndex:
+                self.index = self.firstIndex
 
-'''class Pigeon(pygame.sprite.Sprite):
+class Pigeon(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         size = (20,20)
-        #self.rect = self.get_rect()
         self.pos_x = screen_width
         self.change_x = -5
 
@@ -93,16 +103,18 @@ class Player(pygame.sprite.Sprite):
         self.pos_y = random.randrange(0,screen_height-60)
 
     def update(self):
-        self.pos_x += self.change_x'''
+        self.pos_x += self.change_x
 
+player = Player()
+pigeon = Pigeon()
 #-----Game Loop-----#
 gameRunning = True
 while gameRunning:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        gameRunning = False
-                        pygame.quit()
-                        sys.exit()
-        UpdateBackground()
-        Player.update(Player())
-        pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameRunning = False
+            pygame.quit()
+            sys.exit()
+    UpdateBackground()
+    Player.update(player)
+    pygame.display.flip()
