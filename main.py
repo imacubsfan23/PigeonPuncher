a1e376ce2fbdfda5ebc150628481578b2629abf6
Tim Dickeson <imacubsfan23@gmail.com>
2017-01-15 02:00:50 -0600

    Fully functioning game, 2 points for a hit from the side of the bird (strong) and 1 point for a hit on the top or bottom (weak). Need new background, and to add music/punch noise, then turn into executable for distribution to friends/family who wants it.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, random, os, time
 pygame.init()
 pygame.display.set_caption('Pigeon Puncher')
-__author__ = "Tim Dickeson II"
+__author__ = 'Tim Dickeson II'
 clock = pygame.time.Clock()
 
 White = (255, 255, 255)
@@ -9,7 +9,7 @@ Black = (0, 0, 0)
 Grey = (30, 30, 30)
 
 #-----Background-----#
-bg_path = os.path.join("images", "bg.jpg")
+bg_path = os.path.join('images', 'bg.jpg')
 bg = pygame.image.load(bg_path)
 bg_rect = bg.get_rect()
 
@@ -31,37 +31,39 @@ def UpdateBackground():
         screen.blit(bg,(x,y))
         screen.blit(bg,(x1,y))
         if x <= -screen_width:
-                x = screen_width-scroll_speed #fixes background scrolling issue
+                x = screen_width-scroll_speed
         if x1 <= -screen_width:
                 x1 = screen_width-scroll_speed
 
 #-----Anderson Walking Animations-----#
-'''ATTENTION!!! WALK1 AND WALK2 MUST BE SAME SIZE FOR THIS TO WORK'''
-walk1_path = os.path.join("images", "walk1.png")
+walk1_path = os.path.join('images', 'walk1.png')
 walk1 = pygame.image.load(walk1_path)
 
-walk2_path = os.path.join("images", "walk2.png")
+walk2_path = os.path.join('images', 'walk2.png')
 walk2 = pygame.image.load(walk2_path)
 
 #-----Anderson Jetpack Animations-----#
-jetpack_off_path = os.path.join("images", "jetpack_off.png")
+jetpack_off_path = os.path.join('images', 'jetpack_off.png')
 jetpack_off = pygame.image.load(jetpack_off_path)
 
-jetpack_on_path = os.path.join("images", "jetpack_on.png")
+jetpack_on_path = os.path.join('images', 'jetpack_on.png')
 jetpack_on = pygame.image.load(jetpack_on_path)
 
 #-----Anderson Punch Animation-----#
-punch_pic_path = os.path.join("images", "punch.png")
+punch_pic_path = os.path.join('images', 'punch.png')
 punch_pic = pygame.image.load(punch_pic_path)
 
 #-----Pigeon Flying Animations-----#
-pigeon1_path = os.path.join("images", "pigeon1.gif")
+pigeon1_path = os.path.join('images', 'pigeon1.gif')
 pigeon1 = pygame.image.load(pigeon1_path)
 pigeon1_rect = pigeon1.get_rect()
 
-pigeon2_path = os.path.join("images", "pigeon2.gif")
+pigeon2_path = os.path.join('images', 'pigeon2.gif')
 pigeon2 = pygame.image.load(pigeon2_path)
 pigeon2_rect = pigeon2.get_rect()
+
+#-----Game Icon-----#
+pygame.display.set_icon(punch_pic)
 
 #classes and objects
 class Player(pygame.sprite.Sprite):
@@ -75,7 +77,6 @@ class Player(pygame.sprite.Sprite):
         self.pos_y = floor
         self.rect = pygame.Rect(0, 0, 45, 50)
         self.change_y = 30
-        self.alter_speed = 1
 
     def walk(self):
         temp_time = pygame.time.get_ticks()
@@ -106,6 +107,9 @@ class Player(pygame.sprite.Sprite):
         if player.rect.colliderect(pigeon.rect):
             animation = self.punch()
             pigeon.spawn(pigeon.pos_x, pigeon.pos_y)
+            return True
+        else:
+            return False
 
     def update(self):
         self.pos_y += self.change_y
@@ -115,7 +119,9 @@ class Player(pygame.sprite.Sprite):
             self.pos_y = ceiling
         
         #-START ANIMATIONS-#
-        if self.pos_y >= floor:
+        if self.detectCollision() == True:
+            animation = self.punch()
+        elif self.pos_y >= floor:
             animation = self.walk()
         else:
             if self.change_y < 0:
@@ -137,7 +143,7 @@ class Pigeon(pygame.sprite.Sprite):
         self.firstIndex = 0
         self.lastIndex = 1
         self.pos_x = screen_width
-        self.pos_y = ceiling
+        self.pos_y = floor
         self.rect = pygame.Rect(0, 0, 48, 55)        
         self.change_x = -35
         self.spawn_pixels = screen_width
@@ -166,11 +172,11 @@ class Pigeon(pygame.sprite.Sprite):
 
     def update(self):
         if(self.pos_x<=screen_width-self.spawn_pixels):
-            #gameOver()
-            self.spawn(self.pos_x, self.pos_y)
+            gameRunning = False
+            gameOver()
             self.count_punched -= 1
         self.pos_x += self.change_x
-
+        
         #-START ANIMATIONS-#
         self.rect = pygame.Rect(self.pos_x, self.pos_y, 48, 55)
         self.animate(self.fly())
@@ -191,7 +197,7 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     else:
         pygame.draw.rect(screen, ic,(x,y,w,h))
 
-    smallText = pygame.font.Font("game_font.ttf",20)
+    smallText = pygame.font.Font('game_font.ttf',20)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     screen.blit(textSurf, textRect)
@@ -207,8 +213,7 @@ def gameIntro():
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                gameOverTasks()
         UpdateBackground()
         Player.update(player)
         largeText = pygame.font.Font('game_font.ttf', 60)
@@ -230,9 +235,10 @@ def gameLoop():
         UpdateBackground()
         Pigeon.update(pigeon)
         Player.update(player)
-        if times_ran != 0:
+        if times_ran > 0:
             display_score('Score: ' + str(pigeon.count_punched))
         else:
+            pigeon.count_punched = 0
             display_score('Score: 0')
         pygame.display.flip()
         clock.tick(60)
@@ -242,17 +248,49 @@ def gameLoop():
             if event.type == pygame.KEYUP:
                 player.change_y *= -1
             if event.type == pygame.QUIT:
-                gameOver()
+                gameOverTasks()
         times_ran += 1
+
+def restartGameLoop():
+    global times_ran
+    times_ran = 0
+    Pigeon.count_punched = 0
+    Pigeon.spawn(pigeon, screen_width, floor)
+    gameLoop()
 
 def gameOver():
     gameRunning = False
+    while not gameRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameOverTasks()
+        UpdateBackground()
+        Player.update(player)
+        largeText = pygame.font.Font('game_font.ttf', 40)
+        TextSurf, TextRect = text_objects('Game Over. You scored: ' + str(pigeon.count_punched), largeText)
+        TextRect.center = ((screen_width/2),(screen_height/2))
+        screen.blit(TextSurf, TextRect)
+        button('Try Again',
+               TextRect.center[0]-175, TextRect.center[1]+50,
+               100, 50,
+               Black, Grey,
+               restartGameLoop)
+        button('Quit',
+               TextRect.center[0]+75, TextRect.center[1]+50,
+               100, 50,
+               Black, Grey,
+               gameOverTasks)
+        pygame.display.flip()
+        clock.tick(120)
+
+def gameOverTasks():
     pygame.quit()
     quit()
 
 def main():
     gameIntro()
     gameLoop()
+    gameOver()
 
 if __name__ == '__main__':
     main()
